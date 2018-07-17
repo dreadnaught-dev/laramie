ca817c363da0c524792aed61785b2c26eba4a69e
Preston Clark <preston@dreadnaught.io>
2018-07-17 01:10:48 +0000

    Update select field to not require a value for non-required fields

--- src/resources/views/partials/fields/edit/select.blade.php
+++ src/resources/views/partials/fields/edit/select.blade.php
@@ -3,6 +3,9 @@
 @section('input')
     <span class="select">
         <select id="{{ $field->id }}" name="{{ $field->id }}" {{ $field->extra }} {{ $field->required ? 'required' : '' }}>
+            @if (!$field->required)
+                <option value="">Select {{ strtolower($field->label) }}...</option>
+            @endif
             @foreach (object_get($field, 'options') as $option)
                 <option value="{{ $option }}" {!! $option == object_get($item, $fieldKey) ? 'selected="selected"' : '' !!}>{{ $option }}</option>
             @endforeach
