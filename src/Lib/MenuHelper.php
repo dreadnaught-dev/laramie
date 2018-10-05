e9315c069f558a4b61ef550b023f9ea09bc28eab
Preston Clark <preston@dreadnaught.io>
2018-10-05 11:37:13 +0000

    Adding a menu helper to consolidate dfs actions

---
+++ src/Lib/MenuHelper.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Laramie\Lib;
+
+class MenuHelper
+{
+    protected $menu = null;
+    protected $user = null;
+    protected $currentRoute = null;
+
+    public function __construct($menu, $user, $currentRoute = null)
+    {
+        $this->menu = json_decode(json_encode($menu)); // clone only superficially does what I want
+        $this->user = $user;
+        $this->currentRoute = $currentRoute;
+        $this->setVisibility($this->menu);
+        //dd($this->menu);
+    }
+
+    private function setVisibility($node)
+    {
+        $node->isVisible = false;
+        foreach ($node as $key => $value) {
+            $itemIsVisible = false;
+            switch (gettype($value)) {
+                case 'string':
+                    $itemIsVisible = $this->user->isSuperAdmin
+                       || $this->user->isAdmin
+                       || in_array($value, $this->user->abilities);
+
+                    $node->$key = (object) [
+                        'isLeaf' => true,
+                        'modelName' => $value,
+                        'isVisible' => $itemIsVisible,
+                    ];
+                    break;
+
+                case 'object':
+                    $node->isVisible |= $this->setVisibility($value);
+                    break;
+            }
+            $node->isVisible = (bool) ($node->isVisible || $itemIsVisible);
+        }
+
+        return (bool) $node->isVisible;
+    }
+
+    public function printMenu($node = null)
+    {
+        if ($node === null) {
+            $node = $this->menu;
+        }
+
+        if (object_get($node, 'isVisible') === false) {
+            return;
+        }
+
+        foreach ($node as $key => $value) {
+            if ($key == 'isVisible' || object_get($value, 'isVisible') === false) {
+                continue;
+            }
+            if (!object_get($value, 'isLeaf')) {
+                echo '<li><a>'.$key.'</a><ul>';
+                $this->printMenu($value);
+                echo '</ul></li>';
+            } else {
+                $isActive = false;// $currentRoute->hasParameter('modelKey') && $currentRoute->parameter('modelKey') == $modelKeyOrChild;
+                echo '<li><a class="'.($isActive ? 'is-active' : '').'" href="'.route('laramie::list', ['modelKey' => object_get($value, 'modelName')]).'">'.$key.'</a></li>';
+            }
+        }
+    }
+}
