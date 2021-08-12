c6f1f8b53862b3c9927c6ad724edde1952766262
Joseph Barhorst <joe@dreadnaught.io>
2021-08-12 12:19:43 -0500

    added a query method to laramie models

--- src/Lib/LaramieModel.php
+++ src/Lib/LaramieModel.php
@@ -498,6 +498,11 @@ class LaramieModel implements \JsonSerializable
         return static::setOption('filterQuery', $isFilterQuery);
     }
 
+    public static function query()
+    {
+        return static::getLaramieQueryBuilder('query');
+    }
+
     public static function setOption(string $optionName, $optionValue)
     {
         return static::getLaramieQueryBuilder('setOption', [$optionName, $optionValue]);
